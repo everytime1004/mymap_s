c427dedd7abf62690343d1631551f5512afa0ad3
everytime1004 <anyday1004@gmail.com>
2016-08-19 17:22:39 +0900

    structure2

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -10,11 +10,40 @@ class Api::V1::ToursController < ActionController::Base
   end
 
   def destroy
-  	
+
   end
 
   def show
 
+  end
+
+  protected
+
+  def render_create_success
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "",
+                      :data => }
+    
+  end
+
+  def render_create_error_not_confirmed
+    render json: {
+      success: false,
+      errors: [ I18n.t("devise_token_auth.sessions.not_confirmed", email: @resource.email) ]
+    }, status: 401
+  end
+
+  def render_destroy_success
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "로그아웃 되었습니다." }
+  end
+
+  def render_destroy_error
+    render json: {
+      errors: [I18n.t("devise_token_auth.sessions.user_not_found")]
+    }, status: 404
   end
 
 
--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,10 +1,49 @@
 class Api::V1::TripsController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
   
-  def show
-  	
+  def new
 
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def show
+
+  end
+
+  protected
+
+  def render_create_success
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "",
+                      :data => }
     
+  end
+
+  def render_create_error_not_confirmed
+    render json: {
+      success: false,
+      errors: [ I18n.t("devise_token_auth.sessions.not_confirmed", email: @resource.email) ]
+    }, status: 401
+  end
+
+  def render_destroy_success
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "" }
+  end
+
+  def render_destroy_error
+    render json: {
+      errors: [I18n.t("devise_token_auth.sessions.user_not_found")]
+    }, status: 404
   end
 
 end
