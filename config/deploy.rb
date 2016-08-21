77dff9f74ac81e556f5a7fd7d2b9adb1e5f83b92
everytime1004 <anyday1004@gmail.com>
2016-08-21 21:33:28 +0900

    asd

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,6 @@ set :branch, "master"
 set :use_sudo, false
 set :bundle_binstubs, nil
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', )
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/puma.rb', 'config/application.yml')
 
 after 'deploy:publishing', 'deploy:restart'
 
@@ -24,6 +23,7 @@ namespace :deploy do
             upload! "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
             upload! "config/database.yml", "#{shared_path}/config/database.yml", via: :scp
             upload! "config/puma.rb", "#{shared_path}/config/puma.rb", via: :setup_config
+            set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/puma.rb', 'config/application.yml')            set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/puma.rb', 'config/application.yml')
         end
     end
 
