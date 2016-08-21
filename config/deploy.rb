5720edefc240e5ee6f1a15be79e35b872b40fc62
everytime1004 <anyday1004@gmail.com>
2016-08-21 21:27:03 +0900

    change puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
     before 'check:linked_files', 'puma:config'
-    after 'puma:smart_restart', 'nginx:restart'
+    # after 'puma:smart_restart', 'nginx:restart'
 
     task :check do
         task :linked_files do
@@ -31,58 +31,58 @@ namespace :deploy do
     end
 
     # task :symlink do
-    # 	desc "Symlink application.yml to the release path"
+    #   desc "Symlink application.yml to the release path"
     #     on roles(:app) do
     #         execute "ln -sf #{shared_path}/config/application.yml #{release_path}/config/application.yml"
     #     end
     # end
-	task :restart do
+    task :restart do
         # invoke 'unicorn:legacy_restart'
     end
-	
-	task :printenv do 
-		run "printenv"
-	end
+    
+    task :printenv do 
+        run "printenv"
+    end
 end
 
  
 # namespace :deploy do
-# 	%w[start stop restart].each do |command|
-# 		if command == 'restart'
-# 			task :restart do
-# 				on primary roles :app do
-# 					invoke 'unicorn:reload'
-# 				end
-#   			end
-#   		else
-# 		    desc "#{command} unicorn server"
-# 		    task command, roles: :app, except: {no_release: true} do
-# 		      run "/etc/init.d/unicorn_#{application} #{command}"
-# 		    end
-# 		end
-#   	end
+#   %w[start stop restart].each do |command|
+#       if command == 'restart'
+#           task :restart do
+#               on primary roles :app do
+#                   invoke 'unicorn:reload'
+#               end
+#               end
+#           else
+#           desc "#{command} unicorn server"
+#           task command, roles: :app, except: {no_release: true} do
+#             run "/etc/init.d/unicorn_#{application} #{command}"
+#           end
+#       end
+#       end
  
-#   	task :setup_config, roles: :app do
-#     	sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-#     	sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-#     	run "mkdir -p #{shared_path}/config"
-#     	put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-#     	puts "Now edit the config files in #{shared_path}."
-#   	end
-#   	after "deploy:setup", "deploy:setup_config"
+#       task :setup_config, roles: :app do
+#       sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#       sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+#       run "mkdir -p #{shared_path}/config"
+#       put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+#       puts "Now edit the config files in #{shared_path}."
+#       end
+#       after "deploy:setup", "deploy:setup_config"
  
-#   	task :symlink_config, roles: :app do
-#     	run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-#  	end
-#   	after "deploy:finalize_update", "deploy:symlink_config"
+#       task :symlink_config, roles: :app do
+#       run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#   end
+#       after "deploy:finalize_update", "deploy:symlink_config"
  
-#  	desc "Make sure local git is in sync with remote."
-#   	task :check_revision, roles: :web do
-#    		unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#       	puts "WARNING: HEAD is not the same as origin/master"
-#       	puts "Run `git push` to sync changes."
-#       	exit
-#     	end
-#   	end
-#   	before "deploy", "deploy:check_revision"
+#   desc "Make sure local git is in sync with remote."
+#       task :check_revision, roles: :web do
+#           unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#           puts "WARNING: HEAD is not the same as origin/master"
+#           puts "Run `git push` to sync changes."
+#           exit
+#       end
+#       end
+#       before "deploy", "deploy:check_revision"
 # end
