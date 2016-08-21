e925e5443e6b7187d2eb42f1265ec6d1a3fa5881
everytime1004 <anyday1004@gmail.com>
2016-08-21 19:22:58 +0900

    dadf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'notices#index'
 
   namespace :api do
     namespace :v1 do
