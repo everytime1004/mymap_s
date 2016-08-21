a87083865ae594a0c84e708b42951ad561bca094
everytime1004 <anyday1004@gmail.com>
2016-08-21 13:58:23 +0900

    asd

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -8,11 +8,11 @@ preload_app true
 timeout 30
 
 # Path for the Unicorn socket
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Set path for logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+stderr_path "#{shared_dir}/tmp/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/tmp/log/unicorn.stdout.log"
 
 # Set proccess id path
 pid "#{shared_dir}/tmp/pids/unicorn.pid"
